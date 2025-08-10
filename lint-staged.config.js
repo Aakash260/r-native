2dabcdfe49a45eea91d3cb97fcf5d2fc3af9fc8e
Aakash260 <akashnirwan26@gmail.com>
2025-08-10 15:53:26 +0530

    feat: add pre-commit

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,10 +1,10 @@
 export default {
   // Lint & fix JS/TS files with ESLint
-  '**/*.{js,jsx,ts,tsx}': ['eslint --fix', 'prettier --write'],
+  '**/*.{js,jsx,ts,tsx}': ['npm run lint', 'npm run format:check'],
 
   // Format JSON, Markdown, and YAML files
-  '**/*.{json,md,yml,yaml}': ['prettier --write'],
+  '**/*.{json,md,yml,yaml}': ['npm run format:check'],
 
   // Lint & fix CSS/SCSS with Stylelint
-  '**/*.{css,scss}': ['stylelint --fix', 'prettier --write'],
+  '**/*.{css,scss}': ['npm run lint:stylelint', 'npm run format:check'],
 };
